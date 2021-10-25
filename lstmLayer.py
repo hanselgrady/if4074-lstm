b134ca6a50f8d8ef530e29441fdb19fd04276f88
Hansel Grady <13518140@std.stei.itb.ac.id>
2021-10-25 21:56:45 +0700

    init bias

--- lstmLayer.py
+++ lstmLayer.py
@@ -1,7 +1,15 @@
+import numpy as np
+from functions import Functions
+
 class LSTMLayer:
-    def __init__(self, n_input, n_cell):
-        self.n_input = n_input
+    def __init__(self, size, n_cell):
+        self.size = size
         self.n_cell = n_cell
+        self.parameter = {}
+        self.parameter['bf'] = np.zeros((self.size, 1))
+        self.parameter['bi'] = np.zeros((self.size, 1))
+        self.parameter['bc'] = np.zeros((self.size, 1))
+        self.parameter['bo'] = np.zeros((self.size, 1))
     
     def cellState(self):
         print('cellState')
