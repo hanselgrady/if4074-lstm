67a5bdf750f82ff7bbf712fc1892cd168d815df0
Hansel Grady <13518140@std.stei.itb.ac.id>
2021-10-26 21:15:14 +0700

    Add forget state equation, init other component'

--- lstmLayer.py
+++ lstmLayer.py
@@ -2,23 +2,28 @@ import numpy as np
 from functions import Functions
 
 class LSTMLayer:
-    def __init__(self, size, n_cell):
+    def __init__(self, size, n_cell, x, target):
         self.size = size
         self.n_cell = n_cell
+        self.c_prev = 0
+        self.h_prev = 0
+        self.x = x # array of array
+        self.target = target # array of number
+
         self.parameter = {}
         self.parameter['bf'] = np.zeros((self.size, 1))
         self.parameter['bi'] = np.zeros((self.size, 1))
         self.parameter['bc'] = np.zeros((self.size, 1))
         self.parameter['bo'] = np.zeros((self.size, 1))
     
-    def cellState(self):
-        print('cellState')
-    
-    def forgetGate(self):
-        print('forgetGate')
+    def forgetGate(self, timestep):
+        self.parameter['ft'] = Functions.sigmoid(np.dot(self.parameter['Uf'], self.x[timestep]) + np.dot(self.parameter['Wf'], self.h_prev) + self.parameter['bf'])
     
     def inputGate(self):
         print('inputGate')
+    
+    def cellState(self):
+        print('cellState')
     
     def outputGate(self):
         print('outputGate')
