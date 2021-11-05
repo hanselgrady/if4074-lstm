aed282218ed92362ab8dc7eb0102c5cbbb5fbb68
muhanggarisfanani <muhanggarisfanani@gmail.com>
2021-11-05 17:06:32 +0700

    input gate, cell state, output gate, & forwardprop

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# if4074-lstm
-FI4074 Advanced Machine Learning Project - Long Short-Term Memory
+Tugas Besar IF4074 Advanced Machine Learning Project - Long Short-Term Memory
+
+Anggota kelompok:
+1. 
+
+Cara menjalankan program:
+
--- lstmLayer.py
+++ lstmLayer.py
@@ -1,15 +1,19 @@
+from typing import ForwardRef
 import numpy as np
 from functions import Functions
 
 class LSTMLayer:
+    # def __init__(self, size, n_cell, vocab_size, x, target):
     def __init__(self, size, n_cell, x, target):
         self.size = size
         self.n_cell = n_cell
+        # self.vocab_size = vocab_size
         self.c_prev = np.zeros((self.n_cell, 1))
         self.h_prev = np.zeros((self.n_cell, 1))
         self.x = x # array of array
         self.target = target # array of number
         self.sigmoid = np.vectorize(Functions.sigmoid) # allow function to receive input in form of vector
+        self.softmax = np.vectorize(Functions.softmax) # allow function to receive input in form of vector
 
         self.parameter = {}
         self.parameter['uf'] = np.random.rand(self.n_cell, self.size)
@@ -26,31 +30,36 @@ class LSTMLayer:
         self.parameter['bi'] = np.zeros((self.n_cell, 1))
         self.parameter['bc'] = np.zeros((self.n_cell, 1))
         self.parameter['bo'] = np.zeros((self.n_cell, 1))
+
+        # self.parameter["wv"] = np.random.randn(self.vocab_size, self.n_cell) * (1.0/np.sqrt(self.vocab_size))
+        # self.parameter["bv"] = np.zeros((self.vocab_size, 1))
     
-    def forgetGate(self, timestep):
-        # print('Uf : ')
-        # print(self.parameter['uf'], end='\n\n')
-        # print('xt : ')
-        # print(self.x[timestep], end='\n\n')
-        # print('Wf : ')
-        # print(self.parameter['wf'], end='\n\n')
-        # print('ht-1 : ')
-        # print(self.h_prev, end='\n\n')
-        # print('bf : ')
-        # print(self.parameter['bf'], end='\n\n')
-        # print('Uf.xt : ')
-        # print(np.dot(self.parameter['uf'], self.x[timestep]), end='\n\n')
-        # print('Wf.ht-1 : ')
-        # print(np.dot(self.parameter['wf'], self.h_prev), end='\n\n')
-        # print('Uf.xt + Wf.ht-1 + bf : ')
-        # print(np.dot(self.parameter['uf'], self.x[timestep]) + np.dot(self.parameter['wf'], self.h_prev) + self.parameter['bf'], end='\n\n')
-        self.parameter['f0'] = self.sigmoid(np.dot(self.parameter['uf'], self.x[timestep]) + np.dot(self.parameter['wf'], self.h_prev) + self.parameter['bf'])
+    def forgetGate(self, t):
+        self.parameter['f'+str(t)] = self.sigmoid(np.dot(self.parameter['uf'], self.x[t]) + np.dot(self.parameter['wf'], self.h_prev) + self.parameter['bf'])
     
-    def inputGate(self):
-        print('inputGate')
+    def inputGate(self, t):
+        self.parameter['i'+str(t)] = self.sigmoid(np.dot(self.parameter['ui'], self.x[t]) + np.dot(self.parameter['wi'], self.h_prev) + self.parameter['bi'])
+        self.parameter['CFlag'+str(t)] = np.tanh(np.dot(self.parameter['uc'], self.x[t]) + np.dot(self.parameter['wc'], self.h_prev) + self.parameter['bc'])
     
-    def cellState(self):
-        print('cellState')
+    def cellState(self, t):
+        self.parameter['C'+str(t)] = np.multiply(self.parameter['f'+str(t)], self.c_prev) + np.multiply(self.parameter['i'+str(t)], self.parameter['CFlag'+str(t)])
     
-    def outputGate(self):
-        print('outputGate')
+    def outputGate(self, t):
+        self.parameter['o'+str(t)] = self.sigmoid(np.dot(self.parameter['uo'], self.x[t]) + np.dot(self.parameter['wo'], self.h_prev) + self.parameter['bo'])
+        self.parameter['h'+str(t)] = np.multiply(self.parameter['o'+str(t)], np.tanh(self.parameter['C'+str(t)]))
+
+    def forwardProp(self, t):
+        self.forgetGate(t)
+        self.inputGate(t)
+        self.cellState(t)
+        self.outputGate(t)
+
+        h = self.parameter['h'+str(t)]
+        # v = np.dot(self.parameter["wv"], h) + self.parameter["bv"]
+        # y_hat = self.softmax(v)
+
+        # return y_hat
+        return h
+
+    def predict(self, t):
+        return self.forwardProp(t)
